56fdf25c293ff58d1537aff75b007da1b675aa4e
Remo-L5 <remoleone@icloud.com>
2025-08-06 15:26:54 -0400

    Update main.tf

--- examples/mca/main.tf
+++ examples/mca/main.tf
@@ -10,7 +10,7 @@ module "ip_calc" {
 }
 
 resource "azapi_resource" "invoice_section" {
-  type      = "Microsoft.Billing/invoiceSections@2024-04-01"
+  type      = "Microsoft.Billing/billingAccounts/billingProfiles/invoiceSections@2024-04-01"
   name      = local.invoice_section_name
   parent_id = "/providers/Microsoft.Billing/billingAccounts/${var.billing_account_id}/billingProfiles/${var.billing_profile_id}"
   location  = "global"
